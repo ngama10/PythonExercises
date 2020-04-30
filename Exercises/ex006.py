0998104f2713a0fb5f8ccaeaddccd88fe205cc92
Nuno Gama <nmgama10@gmail.com>
2020-04-30 20:02:28 +0100

    Add ex006

---
+++ Exercises/ex006.py
@@ -0,0 +1,5 @@
+a = input("Type something")
+print("The class of what you type is", type (a) )
+print("Has only spaces?", a.isspace())
+print("Is it a number?", a.isnumeric())
+print("Is it alphabetic?",a.isalpha())
