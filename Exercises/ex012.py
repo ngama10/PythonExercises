35d49fb7be30a178bd5f9535cd3884136f5d164f
Nuno Gama <nmgama10@gmail.com>
2020-05-03 23:24:41 +0100

    Add ex012

---
+++ Exercises/ex012.py
@@ -0,0 +1,4 @@
+n1 = float(input("Write a measurement in meters "))
+cm = n1*100
+mm = n1*1000
+print("The measure you chose in meters,{}, in centimeters will be {} and in milimeters will be {}.".format(n1,cm,mm))
