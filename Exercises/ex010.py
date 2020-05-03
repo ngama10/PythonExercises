f31c801314a95d317efdd56963a57fe9bd42f3be
Nuno Gama <nmgama10@gmail.com>
2020-05-03 22:46:35 +0100

    Add ex010

--- Exercises/ex010.py
+++ Exercises/ex010.py
@@ -1,4 +1,5 @@
-n1 = int(input("Write a number "))
-f = n1+1
-g = n1-1
-print(f"The successor")
+n1 = int(input( "Write a number "))
+d = n1*2
+t = n1*3
+rq = n1**0.5
+print("The double is {}, the triple is {} and the square root is {}".format(d,t,rq))
