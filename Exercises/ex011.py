1a1263fdb91b1aafe6e576206b70ab9f40d6fde5
Nuno Gama <nmgama10@gmail.com>
2020-05-03 22:55:05 +0100

    Add ex011

---
+++ Exercises/ex011.py
@@ -0,0 +1,4 @@
+n1 = float(input("Write the average of the first student "))
+n2 = float(input("Write the average of the second student "))
+m = (n1+n2)/2
+print("The average of the two students is {}".format(m))
