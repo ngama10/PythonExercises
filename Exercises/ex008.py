e59fd611ef66116ebd8420d6417c8d1d63967026
Nuno Gama <nmgama10@gmail.com>
2020-05-03 22:26:48 +0100

    Add ex008

---
+++ Exercises/ex008.py
@@ -0,0 +1,9 @@
+n1 = int(input("Write a number "))
+n2 = int(input("Write another one "))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+print("The sum is {}, the product is {} and the division is {}".format(s, m, d))
+print("The entire division is {} and the exponential is {}".format( di, e))
